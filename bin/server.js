8e1d1f8eebb03c6ed96b682d30202ff49f1fef55
nirmash <nirma@microsoft.com>
2016-09-25 09:56:02 -0700

    initial commit

---
+++ bin/server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+    console.log('Got request for ' + req.url);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    res.end('<h1>Hello App Service Linux!</h1>');
+
+}).listen(process.env.PORT);  
