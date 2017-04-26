52bc59531d9dc37d1739e64d2f9d9419e539ba2b
Stefan <stefsch@microsoft.com>
2017-04-26 15:31:03 -0700

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -6,6 +6,6 @@ http.createServer(function (req, res) {
 
     res.writeHead(200, {'Content-Type': 'text/html'});
 
-    res.end('<h1>Hello App Service Linux!! Hong Kong</h1>');
+    res.end('<h1>Hello App Service Linux!!</h1>');
 
 }).listen(process.env.PORT);  
